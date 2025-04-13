5b47188a6085b49e5365b017940923f5e65e95bd
JTRB <jacobrobertsbaca@gmail.com>
2025-04-12 23:49:28 -0700

    feat: v0.3.12 rename language enum to match extension

--- src/enums.ts
+++ src/enums.ts
@@ -1,6 +1,12 @@
+/**
+ * An enumeration of runnable languages.
+ * 
+ * The string value of each of these is the same as that language's file extension.
+ * For example, the value of `Language.Python` is `"py"`.
+ */
 export enum Language {
   Cpp = "cpp",
-  Python = "python",
+  Python = "py",
 }
 
 export enum RunStatus {
