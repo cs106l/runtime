f19c42980744a705932b5338325febce060f6293
JTRB <jacobrobertsbaca@gmail.com>
2025-05-11 02:00:07 -0700

    feat: add hidden context2d package

---
+++ packages/py/context2d/manifest.ts
@@ -0,0 +1,11 @@
+import { Manifest } from "../../../bundler";
+
+const manifest: Manifest = {
+  name: "context2d",
+  description: "Native support for CanvasRenderingContext2D",
+  version: "0.1.0",
+  files: ["**/*.py"],
+  hidden: true
+};
+
+export default manifest;
