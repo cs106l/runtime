389f6ff7cbabe539f39cec806048c9e106d9a6f1
JTRB <jacobrobertsbaca@gmail.com>
2025-05-13 23:15:34 -0700

    chore: bump stanfordkarel version, hide from public for dev

--- packages/py/stanford-karel/manifest.ts
+++ packages/py/stanford-karel/manifest.ts
@@ -3,9 +3,10 @@ import { Manifest } from "../../../bundler";
 const manifest: Manifest = {
   name: "stanfordkarel",
   description: "Official Stanford Karel library used in CS 106A",
-  version: "0.1.0",
+  version: "0.2.0",
   dependencies: ["canvas"],
-  rootDir: "src"
+  rootDir: "src",
+  hidden: true,
 };
 
 export default manifest;
