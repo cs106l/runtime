18eec35b146dcc75ef8fb4eee90718a407bfac5c
JTRB <jacobrobertsbaca@gmail.com>
2025-05-14 00:03:02 -0700

    fix(stanfordkarel): back to v0.1.0 with new runtime

--- packages/py/stanford-karel/manifest.ts
+++ packages/py/stanford-karel/manifest.ts
@@ -3,7 +3,7 @@ import { Manifest } from "../../../bundler";
 const manifest: Manifest = {
   name: "stanfordkarel",
   description: "Official Stanford Karel library used in CS 106A",
-  version: "0.2.0",
+  version: "0.1.0",
   dependencies: ["context2d"],
   rootDir: "src"
 };
