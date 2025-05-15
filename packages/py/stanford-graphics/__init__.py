d8460629c9cb649b95ad45e9af60739c000862dc
JTRB <jacobrobertsbaca@gmail.com>
2025-05-14 19:11:37 -0700

    feat(stanfordgraphics): param validation and render on cleanup

--- packages/py/stanford-graphics/__init__.py
+++ packages/py/stanford-graphics/__init__.py
@@ -1,7 +1,21 @@
 from abc import ABC, abstractmethod
+from types import NoneType
 from context2d import Context2D
+import weakref
 
 _DEFAULT_OUTLINE_WIDTH = 1
+
+def _param(var, var_types, param_name, function_name):
+    assert type(var) in var_types, (
+        param_name
+        + " should be one of the following types: "
+        + ", ".join([x.__name__ for x in var_types])
+        + " in function "
+        + function_name
+        + ". Recieved "
+        + type(var).__name__
+        + " instead."
+    )
 
 class _Shape(ABC):
     x: float                        # The x coordinate of the shape. Shape-specific meaning.
@@ -45,8 +59,15 @@ class _Shape(ABC):
 
 class _Rectangle(_Shape):
 
-    def __init__(self, x1, y1, x2, y2, fill="black", outline=None, width=1, color=None):
+    def __init__(self, x1, y1, x2, y2, fill="black", outline=None, width=None, color=None):
         super().__init__(x1, y1)
+        _param(x1, [float, int], "x1", "create_rectangle")
+        _param(y1, [float, int], "y1", "create_rectangle")
+        _param(x2, [float, int], "x2", "create_rectangle")
+        _param(y2, [float, int], "y2", "create_rectangle")
+        _param(outline, [str, NoneType], "outline", "create_rectangle")
+        _param(width, [float, int, NoneType], "width", "create_rectangle")
+        _param(color, [str, NoneType], "color", "create_rectangle")
         self.width = x2 - x1
         self.height = y2 - y1
         self.fill = color or fill
@@ -69,20 +90,31 @@ class Canvas:
     __elems: dict[str, _Shape]
 
     def __init__(self, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT):
+        _param(width, [float, int], "width", "Canvas")
+        _param(height, [float, int], "height", "Canvas")
         self.__ctx = Context2D(width=width, height=height)
         self.__elems = {}
 
-    def create_rectangle(self, *args, **kwargs) -> str:
-        return self._create(_Rectangle(*args, **kwargs))
-    
+        # This makes sure that the canvas gets rendered when the object is finally deleted
+        weakref.finalize(self, self.update)
+
     def update(self):
         for elem in self.__elems.values():
             elem.draw(self.__ctx)
         self.__ctx.commit()
 
+    def create_rectangle(self, *args, **kwargs) -> str:
+        return self._create(_Rectangle(*args, **kwargs))
+    
+    def move(self, objectId, dx, dy):
+        _param(objectId, [str], "objectId", "move")
+        _param(dx, [float, int], "dx", "move")
+        _param(dy, [float, int], "dy", "move")
+        if objectId not in self.__elems: return
+        self.__elems[objectId].move(dx, dy)
+
     def _create(self, shape: _Shape) -> str:
         id = f"shape_{Canvas.__next_id}"
         Canvas.__next_id += 1
         self.__elems[id] = shape
-        self.update()
         return id
